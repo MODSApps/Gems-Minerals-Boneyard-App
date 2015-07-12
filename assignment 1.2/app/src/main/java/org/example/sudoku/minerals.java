3e6e7f929e44b484c56dd1602bd40725447cd893
Lindsey Berkowitz <lberkowitz2015@fau.edu>
2015-07-12 18:27:28 -0400

    assignment 1.2

---
+++ assignment 1.2/app/src/main/java/org/example/sudoku/minerals.java
@@ -0,0 +1,53 @@
+package org.example.sudoku;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class minerals extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_minerals);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_minerals, menu);
+        return true;
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+            case R.id.button5:
+                Intent h = new Intent(this, MineralsBoneyard.class);
+                startActivity(h);
+                break;
+
+        }
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
